f09db7ca309f3bf59542ad19f8aae1656a4fead7
Swathi <Swathisp2143@gmail.com>
2022-06-28 02:25:58 +0000

    This is my second commit

--- demo.java
+++ demo.java
@@ -3,6 +3,7 @@ class demo
 	Public static void main(String args [])
 	{
 		System.out.println("Welcome to the world of Git");
+		//COmments gr Git
 	}
 }
 
--- demo1.java
+++ demo1.java
@@ -1,7 +1,6 @@
 Class demo1{
 	Public static void main(String args[]){
                          System.out.println("Demo for Git");
-
+                             System.out.println("modified text");
 	}
 }
-
